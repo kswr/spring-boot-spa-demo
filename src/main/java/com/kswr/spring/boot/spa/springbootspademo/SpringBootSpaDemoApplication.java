78e49c5e45fcadae1b7fc119cfe3d9916b252e33
kswr <ksawery.oleszynski@gmail.com>
2018-10-21 15:00:07 +0200

    Initial commit

---
+++ src/main/java/com/kswr/spring/boot/spa/springbootspademo/SpringBootSpaDemoApplication.java
@@ -0,0 +1,12 @@
+package com.kswr.spring.boot.spa.springbootspademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSpaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSpaDemoApplication.class, args);
+    }
+}
---
+++ src/test/java/com/kswr/spring/boot/spa/springbootspademo/SpringBootSpaDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kswr.spring.boot.spa.springbootspademo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootSpaDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
